2aa8df66d60bc3cb3c53d44afb58ab85567611f0
William A. Wieselquist <ww5@ornl.gov>
2017-01-16 15:29:21 -0500

    debugging message

--- cmake/Darwin/Defaults.cmake
+++ cmake/Darwin/Defaults.cmake
@@ -1,4 +1,5 @@
-SET(CUR_DIR ${SCALE_CMAKE_DIR}/${CMAKE_SYSTEM_NAME})
+SET(flags )
+MESSAGE(STATUS "[BootsOnTheGround] adding Darwin CXX Flags ${flags}")
 
 #
 # Change fortran compiler flags if we have fortran compiler
@@ -6,6 +7,5 @@ SET(CUR_DIR ${SCALE_CMAKE_DIR}/${CMAKE_SYSTEM_NAME})
 IF(${PROJECT_NAME}_ENABLE_Fortran)
    #add compiler preprocessor
    SET(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -cpp")
-
-   INCLUDE(${CUR_DIR}/${CMAKE_Fortran_COMPILER_ID}/Defaults.cmake)
+   INCLUDE(${CMAKE_SOURCE_DIR}/cmake/${CMAKE_SYSTEM_NAME}/${CMAKE_Fortran_COMPILER_ID}/Defaults.cmake)
 ENDIF()
--- cmake/Linux/Defaults.cmake
+++ cmake/Linux/Defaults.cmake
@@ -1,11 +1,11 @@
-SET(CUR_DIR ${SCALE_CMAKE_DIR}/${CMAKE_SYSTEM_NAME})
-
-#add compiler preprocessor
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
+SET(flags "-pthread")
+MESSAGE(STATUS "[BootsOnTheGround] adding Linux CXX Flags ${flags}")
+# add compiler preprocessor
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${flags}")
 
 #
 # Process fortran flags if we have a fortran compiler
 #
 IF(${PROJECT_NAME}_ENABLE_Fortran)
-   INCLUDE(${CUR_DIR}/${CMAKE_Fortran_COMPILER_ID}/Defaults.cmake)
+   INCLUDE(${CMAKE_SOURCE_DIR}/cmake/${CMAKE_SYSTEM_NAME}/${CMAKE_Fortran_COMPILER_ID}/Defaults.cmake)
 ENDIF()
