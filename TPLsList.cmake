f4469dbdeba2349af84ff61f4b8d7a032dee9768
William A. Wieselquist <ww5@ornl.gov>
2017-01-17 18:37:41 -0500

    [added] TPLs/Spdlog

--- TPLsList.cmake
+++ TPLsList.cmake
@@ -1,4 +1,5 @@
 TRIBITS_REPOSITORY_DEFINE_TPLS(
     GTest             TPLs/GTest/FindTPLGTest.cmake                           PT
     BoostFilesystem   TPLs/Boost/Filesystem/FindTPLBoostFilesystem.cmake      PT
+    Spdlog            TPLs/Spdlog/FindTPLSpdlog.cmake                         PT
 )
--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,15 +2,7 @@
 SET( SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
     _GTest            TPLs/GTest            PT OPTIONAL
     _BoostFilesystem  TPLs/Boost/Filesystem PT OPTIONAL
-    _HDF5             TPLs/HDF5             PT OPTIONAL
-    _Curl             TPLs/Curl             PT OPTIONAL
-    _SZip             TPLs/SZip             PT OPTIONAL
-    _ZLib             TPLs/ZLib             PT OPTIONAL
-    _OpenSSL          TPLs/OpenSSL          PT OPTIONAL
-    _GFlags           TPLs/GFlags           PT OPTIONAL
-    _Fmt              TPLs/Fmt              PT OPTIONAL
     _Spdlog           TPLs/Spdlog           PT OPTIONAL
-    _NLJson           TPLs/NLJson           PT OPTIONAL
 )
 
 SET(LIB_REQUIRED_DEP_PACKAGES)
