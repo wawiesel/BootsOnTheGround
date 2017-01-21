9be833378bab56a3981bb8a7fa61da467885dd3d
William A. Wieselquist <ww5@ornl.gov>
2017-01-21 18:01:15 -0500

    [changed] some Flag files

--- cmake/Fortran/GNU/Flags.cmake
+++ cmake/Fortran/GNU/Flags.cmake
@@ -1,1 +1,7 @@
+# Enable C-preprocessor
 BOTG_AddCompilerFlags( Fortran "-cpp" )
+
+# Enable long lines
+BOTG_AddCompilerFlags( Fortran " -ffree-line-length-none" )
+
+BOTG_AddCompilerFlags( Fortran "-pthread" )
