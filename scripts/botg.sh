a4c682d23a3972fc2aa859ae83eea0cb6f473132
William A. Wieselquist <ww5@ornl.gov>
2017-02-03 18:03:37 -0500

    [updated] scripts

--- scripts/botg.sh
+++ scripts/botg.sh
@@ -4,7 +4,7 @@
 BOTG_SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
 #run sequence of operations
-$BOTG_SCRIPT_DIR/botg-config.sh && \
+$BOTG_SCRIPT_DIR/botg-config.sh "$1" && \
 $BOTG_SCRIPT_DIR/botg-build.sh && \
 $BOTG_SCRIPT_DIR/botg-test.sh
 
