9b6559aefc8de3b72cfe6a7d401ce90eace5b91e
William A. Wieselquist <ww5@ornl.gov>
2017-02-03 17:52:43 -0500

    [fixed] source_me.sh should not have exit

--- scripts/source_me.sh
+++ scripts/source_me.sh
@@ -1,6 +1,10 @@
 #This script adds the scripts PATH to the environment so that the
 #BootsOnTheGround scripts can be used.
 called=$_
-[[ $called != $0 ]] || echo "source_me.sh must be sourced!" && exit
-BOTG_SCRIPT_DIR=$(cd $(dirname $BASH_SOURCE) && pwd)
-export PATH=BOTG_SCRIPT_DIR:$PATH
+if [[ $called != $0 ]];
+then
+    BOTG_SCRIPT_DIR=$(cd $(dirname $BASH_SOURCE) && pwd)
+    export PATH=BOTG_SCRIPT_DIR:$PATH
+else
+    echo "source_me.sh must be sourced!"
+fi
